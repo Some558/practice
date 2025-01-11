c5aca205d6b9a14634daa54f15e75d9ac489ac09
Some558 <1641043@gmail.com>
2025-01-11 20:23:04 +0900

    5-1

--- JAVA学習用/配列.java
+++ JAVA学習用/配列.java
@@ -1,5 +1,15 @@
-public class Main {
+public class Main { 
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      introduceOneself();
     }
-  }
+    public static void introduceOneself() {
+      String name = "湊雄輔";
+      int age = 22;
+      double height = 169.9;
+      char zodiac = '辰';
+      System.out.println("私の名前は" + name + "です。");
+      System.out.println("歳は" + age + "です。");
+      System.out.println("身長は" + height + "cmです。");
+      System.out.println("十二支は" + zodiac + "です。");
+    }
+  } 
